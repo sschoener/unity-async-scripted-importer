521b0f2ec7c1ff1121747f52f400bf979cadebe1
Sebastian Schöner <mail@s-schoener.com>
2024-11-18 11:22:55 +0100

    Cleanup importer code a little

--- Assets/Editor/MyScriptableObjectImporter.cs
+++ Assets/Editor/MyScriptableObjectImporter.cs
@@ -16,7 +16,6 @@ public class MyScriptableObjectImporter : ScriptedImporter
         // This also has another side effect: This load here reads data from disk. If you modify your object in memory
         // but then don't save it to disk, the importer won't see the new version.
         MyScriptableObject obj = AssetDatabase.LoadAssetAtPath<MyScriptableObject>(ctx.assetPath);
-        var outputPath = ctx.GetOutputArtifactFilePath($"{obj.name}.json");
 
         // simulate a slow import
         Thread.Sleep(1000);
@@ -26,9 +25,10 @@ public class MyScriptableObjectImporter : ScriptedImporter
         // we don't have to declare a dependency on it. That only works before we're working with GUIDs here, and the
         // GUID is already used in the scriptable object we are importing to reference the texture. So if the GUID changes,
         // our scriptable object would also have to change, hence no need to declare a dependency.
-        AssetDatabase.GUIDFromAssetPath(AssetDatabase.GetAssetPath(obj.MyTexture));
         AssetDatabase.TryGetGUIDAndLocalFileIdentifier(obj.MyTexture, out string guid, out long index);
         string outputJson = $"{{\"MyTexture\": \"{guid}:{index}\",\"MyString\": \"{obj.MyString}\"}}";
+        
+        var outputPath = ctx.GetOutputArtifactFilePath($"{obj.name}.json");
         File.WriteAllText(outputPath, outputJson);
     }
 }
